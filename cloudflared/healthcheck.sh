54429a6da73f9a26c878b132efa91226098d7a29
WoHal <socianer@163.com>
2025-08-12 16:43:58 +0800

    add cloudflared

---
+++ cloudflared/healthcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+res=$(wget -O- -q 127.0.0.1:20241/ready)
+st=$(echo res | grep -Po '"status":\d+' | grep -Po '\d+')
+
+if [ "$st" != "200" ] && [ "" != "$TUNNEL_PIDFILE" ]; then
+	kill -19 $(cat $TUNNEL_PIDFILE)
+fi
