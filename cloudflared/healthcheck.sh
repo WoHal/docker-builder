1ec27c8ba71a174c4df4f51cc4dc5004c6b5df99
WoHal <socianer@163.com>
2025-08-16 07:58:23 +0800

    update cloudflared conf

--- cloudflared/healthcheck.sh
+++ cloudflared/healthcheck.sh
@@ -4,5 +4,5 @@ res=$(wget -O- -q 127.0.0.1:20241/ready)
 st=$(echo res | grep -Po '"status":\d+' | grep -Po '\d+')
 
 if [ "$st" != "200" ] && [ "" != "$TUNNEL_PIDFILE" ]; then
-	kill -19 $(cat $TUNNEL_PIDFILE)
+	kill -9 $(cat $TUNNEL_PIDFILE)
 fi
